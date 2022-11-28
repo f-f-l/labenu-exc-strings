77bd197944916a9d50efdafe73ac3b3c30672db7
fabiodelllima <f.f.fabiodelima@gmail.com>
2022-11-28 17:29:41 -0300

    Exercício 2 finalizado

--- exercicio2/script.js
+++ exercicio2/script.js
@@ -1,1 +1,14 @@
-const minhaString = "Eu não sou supersticioso, mas sou um pouco ________.      ";
+const minhaString = 'Eu não sou supersticioso, mas sou um pouco ________.      '
+console.log('1)')
+console.log(minhaString)
+console.log(minhaString.length)
+
+const novaString = minhaString.trim()
+console.log('2)')
+console.log(novaString)
+console.log(novaString.length)
+
+const outraSring = minhaString.replace('________', 'sticioso')
+console.log('3)')
+console.log(outraSring)
+console.log(novaString.length)
