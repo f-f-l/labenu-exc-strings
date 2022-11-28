54c6beb8299e55cf59c9a3cc453c192fdb8a2b34
fabiodelllima <f.f.fabiodelima@gmail.com>
2022-11-28 17:29:57 -0300

    Exercício 3 finalizado

--- exercicio3/script.js
+++ exercicio3/script.js
@@ -1,1 +1,17 @@
-//Crie a const para a frase aqui
+const str = `Jorge tem uma casa verde e com portão azul, com os dizeres: "BOAS VINDAS, mas não deixe o gato sair"`
+console.log(str)
+
+const newStr = str.replace('verde', 'rosa').replace('azul', 'laranja')
+console.log(newStr)
+
+const inc1 = newStr.includes('verde')
+console.log(inc1)
+
+const inc2 = newStr.includes('laranja')
+console.log(inc2)
+
+const strToUpperCase = str.replace(
+  'mas não deixe o gato sair',
+  'mas não deixe o gato sair'.toUpperCase()
+)
+console.log(strToUpperCase)
